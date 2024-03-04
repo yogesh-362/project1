754505aa9c4da52bfccf8a1b61aba8e32d548eb3
yogesh-362 <goswamiyogesh2002@gmail.com>
2024-03-04 18:20:26 +0530

    AutoToken committed by yogesh

--- app3/urls.py
+++ app3/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from app3 import views
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
-from app3.auth import CustomAuthToken
+# from app3.auth import CustomAuthToken
 
 router = DefaultRouter()
 
@@ -11,5 +11,5 @@ router.register('stuapi', views.StudentModelViewSet, basename='student'),
 urlpatterns = [
     path('', include(router.urls)),
     path('auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('gettoken/', CustomAuthToken.as_view())
+    # path('gettoken/', CustomAuthToken.as_view())
 ]
