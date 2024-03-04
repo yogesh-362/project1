df2496c607a914336b10f2be2e1890f806d8b8af
yogesh-362 <goswamiyogesh2002@gmail.com>
2024-03-04 17:10:38 +0530

    project10 committed by yogesh

---
+++ app3/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from app3 import views
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
+from app3.auth import CustomAuthToken
+
+router = DefaultRouter()
+
+router.register('stuapi', views.StudentModelViewSet, basename='student'),
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('gettoken/', CustomAuthToken.as_view())
+]
